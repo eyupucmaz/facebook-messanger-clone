faa82e30f5b8bd5a96c9c4169c69acbf64ceb84b
eyupucmaz <eyupucmaz@gmail.com>
2020-09-25 17:59:34 +0300

    building chat app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Message from "./components/Message";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState("");
+	const [messages, setMessages] = useState([
+		{
+			username: "eyup",
+			text: "HEllo everyone",
+		},
+		{
+			username: "everyone",
+			text: "hey eyup",
+		},
+		{
+			username: "dummy",
+			text: "dummy message",
+		},
+	]);
+	const [username, setUsername] = useState("");
+
+	useEffect(() => {
+		setUsername(prompt("Please enter your name"));
+	}, []);
+
+	const sendMessage = (event) => {
+		event.preventDefault();
+		//! All the logic to send a  message goes here
+		setMessages([...messages, { username: username, text: input }]);
+		setInput("");
+	};
+
+	return (
+		<div className="app">
+			<h1>Facebook Messenger App 🚀 </h1>
+			<h3>Welcome {username}</h3>
+
+			<form className="app__form">
+				<FormControl className="app__formInput">
+					<InputLabel>Enter a message...</InputLabel>
+					<Input
+						type="text"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+					/>
+				</FormControl>
+
+				<Button
+					className="app_formButton"
+					variant="contained"
+					color="primary"
+					type="submit"
+					disabled={!input}
+					onClick={sendMessage}
+				>
+					Send
+				</Button>
+			</form>
+			<div className="app_messages">
+				{messages?.map((obj, index) => (
+					<Message key={index} message={obj.text} username={obj.username} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
